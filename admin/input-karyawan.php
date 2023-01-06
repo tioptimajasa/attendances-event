3fea1edf1060b16b0ae74b1e8ac19bbef85b2f9d
rizkidiansaputro <rizkidiansaputro@gmail.com>
2023-01-06 09:17:55 +0700

    updates footer,head,header,sidecontrol to global-templates (admin level)

--- admin/input-karyawan.php
+++ admin/input-karyawan.php
@@ -1,11 +1,11 @@
 <?php include "session.php"; ?>
 <!DOCTYPE html>
 <html>
-  <?php include "head.php"; ?>
+  <?php include "../global-templates/head.php"; ?>
   <body class="hold-transition skin-green-light sidebar-mini">
     <div class="wrapper">
 
-      <?php include "header.php"; ?>
+      <?php include "../global-templates/header.php"; ?>
       <!-- Left side column. contains the logo and sidebar -->
       <?php include "menu.php"; ?>
 
@@ -139,9 +139,9 @@
 
         </section><!-- /.content -->
       </div><!-- /.content-wrapper -->
-      <?php include "footer.php"; ?>
+      <?php include "../global-templates/footer.php"; ?>
 
-      <?php include "sidecontrol.php"; ?>
+      <?php include "../global-templates/sidecontrol.php"; ?>
       <!-- Add the sidebar's background. This div must be placed
            immediately after the control sidebar -->
       <div class="control-sidebar-bg"></div>
